f54f348aff1bca7f30d3cdb8b9a2dd06463d4d50
Bodor Gergely <bodorgergely91@gmail.com>
2017-10-05 00:42:07 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var data = [
+    {
+        id: 0,
+        text: 'enhancement'
+    },
+    {
+        id: 1,
+        text: 'bug'
+    },
+    {
+        id: 2,
+        text: 'duplicate'
+    },
+    {
+        id: 3,
+        text: 'invalid'
+    },
+    {
+        id: 4,
+        text: 'wontfix'
+    }
+];
+
+$('#select2').select2({
+    width: 'resolve',
+    placeholder: 'Select an option',
+    data: data
+});
